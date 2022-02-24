991c52abad51401bec672c5dddcf9e14325a29c3
terryjo-blyth <terryjoblyth94@gmail.com>
2022-02-25 11:51:10 +1300

    testing-auth0

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
@@ -12,12 +13,20 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>,
-    </Provider>,
+  ReactDOM.render(
+    <Auth0Provider
+      domain={'https://tohora22-terryjo.au.auth0.com'}
+      clientId={'p8ucQhswnlxpukdQDwq0WeSgBm16pnSK'}
+      redirectUri={window.location.origin}
+      audience='https://whare/api'
+    >
+
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </Auth0Provider>,
     document.getElementById('app')
   )
 })
