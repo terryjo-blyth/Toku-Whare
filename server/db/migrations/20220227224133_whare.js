085089a16c8eaeed8f845a445d93ef9eb44cf06d
terryjo-blyth <terryjoblyth94@gmail.com>
2022-02-28 14:07:11 +1300

    update-database

--- server/db/migrations/20220227224133_whare.js
+++ server/db/migrations/20220227224133_whare.js
@@ -7,7 +7,7 @@ exports.up = function (knex) {
     table.increments('id')
     table.string('description')
     table.string('questionPrompts')
-    table.integer('color')
+    table.string('color')
     table.string('resourceLinks')
   })
 }
