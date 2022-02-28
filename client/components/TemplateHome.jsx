2ce46ae04b2c59475671c2cc1faefcea0f864c68
slairdy <sacha.j.laird@gmail.com>
2022-03-01 09:43:50 +1300

    styling aspect page

--- client/components/TemplateHome.jsx
+++ client/components/TemplateHome.jsx
@@ -8,7 +8,7 @@ function TemplateHome () {
       <nav className="aspectsNav">
         <a className="wairua" href="/tahawairua">Taha Wairua <br /><small>Spiritual</small></a>
         <a className="tinana" href="/tahatinana">Taha Tinana <br /><small>Our Body</small></a>
-        <a className="hinegaro" href="/tahahinengaro">Taha Hinengaro <br /><small>Mind and emotions</small></a>
+        <a className="hinengaro" href="/tahahinengaro">Taha Hinengaro <br /><small>Mind and emotions</small></a>
         <a className="whanau" href="/tahawhanau">Taha Whānau <br /><small>Family and Friends</small></a>
         <a className="whenua" href="/whenua">Whenua <br /><small>Land/Roots</small></a>
       </nav>
