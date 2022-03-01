726343c5b6e60ea387087d76cacf86909e72d35b
slairdy <sacha.j.laird@gmail.com>
2022-03-01 13:50:20 +1300

    styling

--- client/components/TemplateUser.jsx
+++ client/components/TemplateUser.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import Avatar from './Avatar'
 import Hairstyles from './Hairstyles'
 
+const hairColorArr = [
+  '29abe2',
+  '22b573',
+  'ff00ff',
+  'f7931e',
+  'f7e96f',
+  '754c24',
+  '000000'
+]
+
+const headColourArr = [
+  'f1c0a5',
+  'd29367',
+  'ba8041',
+  '995d34',
+  '59331a'
+]
+
 function TemplateUser () {
   return (
     <section id="userPage">
@@ -11,8 +29,11 @@ function TemplateUser () {
         <input type="text" name="name" id="" />
         <label htmlFor="dob">My Birthday</label>
         <input type="date" name="dob" id="" />
-        <Avatar />
-        <Hairstyles />
+        <h3>My Avatar</h3>
+        <section id="chooseAvatar">
+          <Avatar />
+          <Hairstyles />
+        </section>
         <button id="delete">Delete &times;</button>
         <button id="save">Save</button>
       </form>
