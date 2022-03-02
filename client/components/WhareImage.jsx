9a0788a4fad47a31e5ad0ac46c68af0c00f6fb98
slairdy <sacha.j.laird@gmail.com>
2022-03-02 20:29:02 +1300

    minor image changes

--- client/components/WhareImage.jsx
+++ client/components/WhareImage.jsx
@@ -5,52 +5,58 @@ function WhareImage () {
     <section className="whareContainer">
 
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="whareBg" className="st0">
-          <polygon className="st1" points="632.6,225.2 530,160.4 530,104.8 490,104.8 490,135.1 346.5,44.4 346.5,10.3 298.5,10.3 298.5,44
-    153,135.9 153,104.8 113,104.8 113,161.2 11.4,225.4 34.4,261.7 112.9,212.1 112.9,404.8 113,404.8 113,435.8 530,435.8 530,404.8
-    530,403.8 530,211.2 609.6,261.6" />
+        <g id="whareBg">
+          <polygon className="st0" points="632.6,225.2 530,160.4 530,104.8 490,104.8 490,135.1 346.5,44.4 346.5,10.3 298.5,10.3 298.5,44
+		153,135.9 153,104.8 113,104.8 113,161.2 11.4,225.4 34.4,261.7 112.9,212.1 112.9,368 113,368 113,399 530,399 530,368 530,367
+		530,211.2 609.6,261.6 	"/>
         </g>
       </svg>
 
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="paepae" className="st0">
-          <rect x="113" y="403.8" className="st2" width="417" height="32" />
+        <g id="paepae" className="st1">
+          <rect x="113" y="367" className="st2" width="417" height="32"/>
         </g>
       </svg>
 
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="maihi-l" className="st0">
-          <polygon className="st4" points="298.5,44 11.4,225.4 34.4,261.7 298.5,94.8" />
+        <g id="maihi-l" className="st1">
+          <polygon className="st5" points="298.5,44 11.4,225.4 34.4,261.7 298.5,94.8 	"/>
         </g>
       </svg>
 
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="maihi-r" className="st0">
-          <polygon className="st5" points="346,44.1 632.6,225.2 609.6,261.6 346,95" />
+        <g id="maihi-r" className="st1">
+          <polygon className="st6" points="346,44.1 632.6,225.2 609.6,261.6 346,95 	"/>
         </g>
       </svg>
 
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="po-l" className="st0">
-          <rect x="113" y="104.8" className="st6" width="40" height="300" />
+        <g id="po-l" className="st1">
+          <rect x="113" y="104.8" className="st7" width="40" height="263.3"/>
         </g>
       </svg>
 
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="po-r" className="st0">
-          <rect x="490" y="104.8" className="st7" width="40" height="300" />
+        <g id="po-r" className="st1">
+          <rect x="490" y="104.8" className="st8" width="40" height="263.3"/>
         </g>
       </svg>
 
       <svg id="tekoteko" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="tekoteko" className="st0">
-          <rect x="298.5" y="10.3" className="st8" width="48" height="105" />
+        <g id="tekoteko" className="st1">
+          <rect x="298.5" y="10.3" className="st9" width="48" height="105"/>
         </g>
       </svg>
 
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
-        <g id="kuwaha" className="st0">
-          <path className="st3" d="M246.8,209.3v184h149v-184H246.8z M378,371.3H264.6v-140H378V371.3z" />
+        <g id="kuwaha" className="st1">
+          <path className="st3" d="M222,215.8V367h106.7V215.8H222z M310.9,345h-71.1V237.8h71.1V345z"/>
+        </g>
+      </svg>
+
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 643 449" style={{ enableBackground: 'new 0 0 643 449' }} xmlSpace="preserve">
+        <g id="matapihi" className="st1">
+          <path className="st4" d="M361.7,215.8V305h99.7v-89.3H361.7z M443.6,283h-64.1v-45.3h64.1V283z"/>
         </g>
       </svg>
 
